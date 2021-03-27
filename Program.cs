1581e195b057ad17e9b7777eb866066b3cbf77d6
Gerard Gascón <52170489+Geri8@users.noreply.github.com>
2021-03-27 11:04:03 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using System;
+using System.IO;
+
+namespace RomExecuter{
+    class Program{
+        static void Main(string[] args){
+            string appliactionPath = Path.GetDirectoryName(System.AppContext.BaseDirectory);
+            DirectoryInfo di = new DirectoryInfo(appliactionPath);
+            appliactionPath = di.Parent.FullName + @"\out\rom.bin";
+            Process.Start(
+                @"C:\sgdk\emulators\Regen0972\Regen.exe", 
+                appliactionPath
+            );
+        }
+    }
+}
