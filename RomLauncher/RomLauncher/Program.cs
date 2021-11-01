5974b70b40f440ad0e1709c269a5ae5f3e154222
Gerard Gascón <ggasconmoline@gmail.com>
2021-11-01 13:58:55 +0100

    Redid the project structure

---
+++ RomLauncher/RomLauncher/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using System;
+using System.IO;
+
+namespace RomExecuter{
+	class Program{
+		static void Main(string[] args){
+			string appliactionPath = Path.GetDirectoryName(System.AppContext.BaseDirectory);
+			string[] lines = System.IO.File.ReadAllLines(appliactionPath + @"\emulatorPath.csv");
+
+			DirectoryInfo di = new DirectoryInfo(appliactionPath);
+			appliactionPath = di.Parent.FullName + @"\out\rom.bin";
+			Process.Start(@lines[0], appliactionPath);
+		}
+	}
+}
