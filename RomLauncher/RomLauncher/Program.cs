9208ed22be3810b82cf15ef6a4b91364c1bbc4c1
Gerard Gascón <ggasconmoline@gmail.com>
2021-11-01 14:14:49 +0100

    Modified it to be lighter

--- RomLauncher/RomLauncher/Program.cs
+++ RomLauncher/RomLauncher/Program.cs
@@ -2,11 +2,11 @@
 using System;
 using System.IO;
 
-namespace RomExecuter{
+namespace RomLauncher{
 	class Program{
-		static void Main(string[] args){
-			string appliactionPath = Path.GetDirectoryName(System.AppContext.BaseDirectory);
-			string[] lines = System.IO.File.ReadAllLines(appliactionPath + @"\emulatorPath.csv");
+		static void Main(){
+			string appliactionPath = Path.GetDirectoryName(AppContext.BaseDirectory);
+			string[] lines = File.ReadAllLines(appliactionPath + @"\emulatorPath.csv");
 
 			DirectoryInfo di = new DirectoryInfo(appliactionPath);
 			appliactionPath = di.Parent.FullName + @"\out\rom.bin";
