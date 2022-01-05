40b1d34c899f0a201aa33b7680a35c09966bc828
Gerard Gascón <52170489+GerardGascon@users.noreply.github.com>
2022-01-05 17:14:27 +0100

    Update Program.cs

--- RomLauncher/RomLauncher/Program.cs
+++ RomLauncher/RomLauncher/Program.cs
@@ -8,11 +8,11 @@ namespace RomLauncher{
 			string appliactionPath = Path.GetDirectoryName(AppContext.BaseDirectory);
 			string[] lines = File.ReadAllLines(appliactionPath + @"\emulatorPath.txt");
 
-            System.Diagnostics.Process.Start(appliactionPath + @"\delete-blastem.bat");
+			System.Diagnostics.Process.Start(appliactionPath + @"\delete-blastem.bat");
 
-            DirectoryInfo di = new DirectoryInfo(appliactionPath);
+			DirectoryInfo di = new DirectoryInfo(appliactionPath);
 			appliactionPath = di.Parent.FullName + @"\out\rom.bin";
 			Process.Start(@lines[0], appliactionPath);
 		}
-    }
+	}
 }
