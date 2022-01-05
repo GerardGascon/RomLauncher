f68c33476b9630369ee37f99cfd2f97266be93b7
Gerard Gascón <52170489+GerardGascon@users.noreply.github.com>
2022-01-05 17:03:48 +0100

    Update Program.cs

--- RomLauncher/RomLauncher/Program.cs
+++ RomLauncher/RomLauncher/Program.cs
@@ -8,9 +8,40 @@ namespace RomLauncher{
 			string appliactionPath = Path.GetDirectoryName(AppContext.BaseDirectory);
 			string[] lines = File.ReadAllLines(appliactionPath + @"\emulatorPath.txt");
 
+            DeleteBlastem();
+
 			DirectoryInfo di = new DirectoryInfo(appliactionPath);
 			appliactionPath = di.Parent.FullName + @"\out\rom.bin";
 			Process.Start(@lines[0], appliactionPath);
 		}
-	}
+
+        static void DeleteBlastem(){
+            int exitCode;
+            ProcessStartInfo processInfo;
+            Process process;
+
+            processInfo = new ProcessStartInfo("cmd.exe", "/c " + "$RD /S /Q \""+ @"%LOCALAPPDATA%\blastem" + "\"");
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            // *** Redirect the output ***
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            process = Process.Start(processInfo);
+            process.WaitForExit();
+
+            // *** Read the streams ***
+            // Warning: This approach can lead to deadlocks, see Edit #2
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+
+            exitCode = process.ExitCode;
+
+            Console.WriteLine("output>>" + (String.IsNullOrEmpty(output) ? "(none)" : output));
+            Console.WriteLine("error>>" + (String.IsNullOrEmpty(error) ? "(none)" : error));
+            Console.WriteLine("ExitCode: " + exitCode.ToString(), "ExecuteCommand");
+            process.Close();
+        }
+
+    }
 }
